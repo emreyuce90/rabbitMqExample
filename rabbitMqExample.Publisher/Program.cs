714d7d6cbc6ad59702c74d124b5fe2e8929f9e55
Emre Yüce <emreyuce90@hotmail.com>
2023-11-13 16:52:10 +0300

    Direct Exchange Type  implemented

--- rabbitMqExample.Publisher/Program.cs
+++ rabbitMqExample.Publisher/Program.cs
@@ -1,4 +1,5 @@
 ﻿using RabbitMQ.Client;
+using System.Runtime.ConstrainedExecution;
 using System.Text;
 
 
@@ -9,16 +10,41 @@ using var connection = factory.CreateConnection();
 //Creating channel and sending message directly to queue
 var channel = connection.CreateModel();
 //durable, exchange down olsa bile bu exchange tutulmaya devam eder
-channel.ExchangeDeclare(exchange:"logs-fanout",type:ExchangeType.Fanout,durable:true,false,null);
+//exchange: kuyruğa farklı kaynaklardan bağlanılsın mı? 
+//autoDelete:publisher down olduğunda exchange silinsin mi
+channel.ExchangeDeclare(exchange: "logs-direct",durable:true,type:ExchangeType.Direct); //Exchange in oluşturulmasıu
+
+//4 farklı queue nin oluşturulması ve bu queuelerin mevcut exchange bind edilmesi
+Enum.GetNames(typeof(LogTypes)).ToList().ForEach(x => {
+    var routingKey = $"route-{x}"; //route-Critical route-Error...
+    var queueName = $"direct-queue-{x}"; //direct-queue-Critical //direct-queue-Error...
+    channel.QueueDeclare(queue: queueName, durable: true, exclusive: false, autoDelete: false); //kuyruk oluşturulması
+    channel.QueueBind(queueName, "logs-direct", routingKey, null);//bu kuyrukların exchange e bind edilmesi
+});
 
 
+
+//Kuyruklara mesaj gönderme işlemi
 for (int i = 0; i <= 50; i++) {
-    string message = "Log"+i;
+
+    //Rasgele bir rota oluşturulur
+    LogTypes randomTypes = (LogTypes)new Random().Next(1,5);  
+    
+    string message = $"LogType:{randomTypes}";
     var messageBody = Encoding.UTF8.GetBytes(message);
-    channel.BasicPublish(exchange:"logs-fanout","", null, messageBody);
-    Console.WriteLine("Log gönderildi");
+    var routeKey = $"route-{randomTypes}";
+    channel.BasicPublish(exchange: "logs-direct",routeKey, null, messageBody);
+    Console.WriteLine($"Log gönderildi:{message}");
 
 }
 
 Console.ReadLine();
+
+
+public enum LogTypes {
+    Critical = 1,
+    Error = 2,
+    Warning = 3,
+    Info = 4
+}
 
